eeb6621e8541a2c5b22b7ca51946f0d50022080d
ShubhamKumar547 <kumarshubh054@gmail.com>
2025-06-20 15:22:21 +0530

    basic file structure and customization to make it ready to add the features

---
+++ styles/tailwind.config.js
@@ -0,0 +1,19 @@
+// tailwind.config.js
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}', // if using App Router
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-geist-sans)', ...fontFamily.sans],
+        mono: ['var(--font-geist-mono)', ...fontFamily.mono],
+      },
+    },
+  },
+  plugins: [],
+}
